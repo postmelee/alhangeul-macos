4dc2c65ba4f9b38465c00bc90d2583c1bfb4562b
postmelee <meleeisdeveloping@gmail.com>
2026-05-06 13:25:16 +0900

    Task #154 Stage 3: 배포 스크립트와 Cask rename 반영

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -13,9 +13,9 @@ BUILD_ROOT="${ALHANGEUL_BUILD_ROOT:-$ROOT/build.noindex}"
 BUILD_DIR="$BUILD_ROOT/release"
 XCODE_BUILD_DIR="$BUILD_DIR/xcodebuild"
 DERIVED_DATA_DIR="$BUILD_DIR/DerivedData"
-PROJECT_NAME="AlhangeulMac"
-BUILD_APP_NAME="AlhangeulMac.app"
-APP_NAME="AlhangeulMac.app"
+PROJECT_NAME="Alhangeul"
+BUILD_APP_NAME="Alhangeul.app"
+APP_NAME="Alhangeul.app"
 ZIP_NAME="alhangeul-macos-$VERSION.zip"
 LSREGISTER="/System/Library/Frameworks/CoreServices.framework/Versions/Current/Frameworks/LaunchServices.framework/Versions/Current/Support/lsregister"
 
@@ -24,7 +24,7 @@ touch "$BUILD_ROOT/.metadata_never_index"
 rm -rf "$XCODE_BUILD_DIR"
 rm -rf "$DERIVED_DATA_DIR"
 rm -rf "${BUILD_DIR:?}/$BUILD_APP_NAME" "${BUILD_DIR:?}/$BUILD_APP_NAME.dSYM"
-rm -rf "$BUILD_DIR"/AlhangeulMac*.appex "$BUILD_DIR"/AlhangeulMac*.appex.dSYM "$BUILD_DIR"/AlhangeulMac*.swiftmodule
+rm -rf "$BUILD_DIR"/Alhangeul*.appex "$BUILD_DIR"/Alhangeul*.appex.dSYM "$BUILD_DIR"/Alhangeul*.swiftmodule
 rm -rf "$BUILD_DIR/include" "$BUILD_DIR/librhwp.a"
 
 "$ROOT/scripts/build-rust-macos.sh" --verify-lock
